ddbbea35ff0b4eef68985dbe0f2179d839f1745f
Kacper <kacper.moll@gmail.com>
2022-05-14 14:17:00 +0200

    Project prepared for working

---
+++ main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
